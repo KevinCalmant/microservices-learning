bf28a6b828a3bf76808ecb651840dd9c5e6bcbc4
Kévin Calmant <kevin.calmant@outlook.com>
2023-06-27 22:21:47 +0200

    fix loging by using otel deps

--- notification/src/main/java/fr/kecal/kecalservices/notification/v1/consumer/NotificationConsumer.java
+++ notification/src/main/java/fr/kecal/kecalservices/notification/v1/consumer/NotificationConsumer.java
@@ -2,21 +2,16 @@ package fr.kecal.kecalservices.notification.v1.consumer;
 
 import fr.kecal.kecalservices.clients.notification.model.NotificationRequest;
 import fr.kecal.kecalservices.notification.v1.services.NotificationService;
-import io.micrometer.observation.Observation;
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public record NotificationConsumer(NotificationService notificationService, Tracer tracer, ObservationRegistry observationRegistry) {
+public record NotificationConsumer(NotificationService notificationService) {
     @RabbitListener(queues = "${rabbit.queue.notification}")
     public void consumer(NotificationRequest notificationRequest) {
-        Observation.createNotStarted("on-message", this.observationRegistry).observe(() -> {
-            log.info("Consumed {} from queue", notificationRequest);
-            notificationService.saveNotification(notificationRequest);
-        });
+        log.info("Consumed {} from queue", notificationRequest);
+        notificationService.saveNotification(notificationRequest);
     }
 }
